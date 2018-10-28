a89c742037223aa09c340f70747676f9047bcfb1
Ilya Melamed <ilya77@gmail.com>
2018-10-28 17:21:25 +0200

    Link to obs.dll with stub obs.lib

--- main.cpp
+++ main.cpp
@@ -9,103 +9,18 @@
 #include <vector>
 #include <set>
 
-void print_error(LPWSTR label)
+extern "C" __declspec(dllexport) const char *obs_get_version_string(void);
+
+int CheckVersionString()
 {
-	LPVOID lpMsgBuf;
-	LPVOID lpDisplayBuf;
-	DWORD dw = GetLastError();
+	std::cout << obs_get_version_string() << std::endl;
 
-	FormatMessageW(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER |
-		FORMAT_MESSAGE_FROM_SYSTEM |
-		FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,
-		dw,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		(LPWSTR)&lpMsgBuf,
-		0, NULL);
-
-	// Display the error message and exit the process
-
-	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
-		(wcslen((LPCWSTR)lpMsgBuf) + wcslen((LPCWSTR)label) + 40) * sizeof(WCHAR));
-	StringCchPrintfW((LPWSTR)lpDisplayBuf,
-		LocalSize(lpDisplayBuf) / sizeof(WCHAR),
-		L"%s failed with error %d: %s",
-		label, dw, lpMsgBuf);
-
-	OutputDebugStringW(GetCommandLineW());
-	OutputDebugStringW((LPCWSTR)lpDisplayBuf);
-	OutputDebugStringW(L"\n");
-
-	std::cerr << GetCommandLineW() << ":" << (LPCWSTR)lpDisplayBuf << std::endl;
-
-	LocalFree(lpMsgBuf);
-	LocalFree(lpDisplayBuf);
-}
-
-int CheckVersionString(std::wstring path)
-{
-	using convert_type = std::codecvt_utf8<WCHAR>;
-	std::wstring_convert<convert_type, WCHAR> converter;
-
-	int result = 255;
-
-	size_t index = path.find_last_of('\\');
-	std::wstring folder_path = path.substr(0, index);
-	if (!folder_path.size()) {
-		folder_path = L".";
-	}
-
-	if (SetCurrentDirectoryW(folder_path.c_str())) {
-		result = 3;
-
-		HMODULE hModule = LoadLibraryW(path.c_str());
-		if (hModule != NULL) {
-			result = 4;
-
-			FARPROC pProc = GetProcAddress(hModule, "obs_get_version_string");
-
-			if (pProc != NULL) {
-				result = 5;
-
-				typedef char* (*obs_get_version_string_t)();
-
-				char* version_string = ((obs_get_version_string_t)pProc)();
-
-				std::cout << version_string << std::endl;
-
-				result = 0;
-			}
-			else print_error(L"GetProcAddress");
-
-			FreeLibrary(hModule);
-		}
-		else print_error(L"LoadLibraryW");
-	}
-	else print_error(L"SetCurrentDirectoryW");
-
-	return result;
+	return 0;
 }
 
 int wmain(int argc, WCHAR* argv[])
 {
-	int exit_code = 1;
-
-	if (argc != 2) {
-		std::cerr << "Usage: " << argv[0] << " path-to.dll" << std::endl;
-	}
-	else {
-		exit_code = 2;
-
-		std::wstring path = argv[1];
-		
-		int result = CheckVersionString((LPCTSTR)path.c_str());
-
-		if (result == 0) {
-			exit_code = 0;
-		}
-	}
+	int exit_code = CheckVersionString();
 
 	return exit_code;
 }
@@ -124,7 +39,7 @@ int CALLBACK WinMain(
 	sz_arglist = CommandLineToArgvW(GetCommandLineW(), &n_args);
 	if (sz_arglist == NULL)
 	{
-		print_error(L"CommandLineToArgvW");
+		std::cerr << "CommandLineToArgvW" << std::endl;
 	}
 	else
 	{
---
+++ obs/main.cpp
@@ -0,0 +1,4 @@
+extern "C" __declspec(dllexport) const char *obs_get_version_string(void)
+{
+	return "999.0.0";
+}
